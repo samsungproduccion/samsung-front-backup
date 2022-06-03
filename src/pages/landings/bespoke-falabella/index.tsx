0180d092e6a961fd3f9bdc3cca3fb1bee0a577a5
Williams Cordova <69328855+wicomon@users.noreply.github.com>
2022-06-03 15:29:07 -0500

    css fixes

--- src/pages/landings/bespoke-falabella/index.tsx
+++ src/pages/landings/bespoke-falabella/index.tsx
@@ -1,32 +1,24 @@
-import './bespoke-falabella.scss';
-import Cards from './components/Cards';
-import Features from './components/Features';
-import Hero from './components/Hero';
-import ImagesContainer from './components/ImagesContainer';
-import Video from './components/Video';
+import "./bespoke-falabella.scss";
+import Cards from "./components/Cards";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import ImagesContainer from "./components/ImagesContainer";
+import Video from "./components/Video";
 import logo_samsung from "../../../assets/images/svg/svg-logo.svg";
 const BespokeFalabella = () => {
-
-  return(
-    <div className="main--container-fb">
+  return (
+    <div className="main--container-fb" style={{marginBottom: '50px'}}>
       <div className="line">
         <div className="logo-samsung">
           <img src={logo_samsung} alt="logo-samsung" />
         </div>
-        
       </div>
-        <Hero />
-        <Video />
-        <Features />
-        <ImagesContainer />
-        <Cards />
-        <div className="line">
-        <div className="logo-samsung">
-          <img src={logo_samsung} alt="logo-samsung" />
-        </div>
-        
-      </div>
+      <Hero />
+      <Video />
+      <Features />
+      <ImagesContainer />
+      <Cards />
     </div>
   );
-}
+};
 export default BespokeFalabella;
